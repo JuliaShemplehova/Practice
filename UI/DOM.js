dcab7f3a1078e6b5b02e0a2c5e0ce6baf5a45351
Shemplehova Yulia <ms.yulsh@mail.ru>
2018-03-12 19:01:50 +0300

    modefied removePhotoPost

--- UI/DOM.js
+++ UI/DOM.js
@@ -2,76 +2,96 @@ let DOM = (function() {
     let user = 'sealjuli';
     let content = document.createElement('div');
     content.className = 'content';
+    let butt = document.createElement('button');
+    butt.className = 'more';
+    butt.innerHTML = 'Загрузить еще';
     return {
-    	autorizing: function(user) {
-    		if (user!==null) {
-       			let photoUser = document.createElement('img');
-       		    photoUser.className = 'main_photo';
-       		    document.body.appendChild(photoUser);
-       		    let newfont = document.createElement('font');
-       		    newfont.className = 'username';
-       		    newfont.innerHTML = user;
-       		    document.body.appendChild(newfont);
-       		    let container = document.createElement('div');
-       		    container.innerHTML = '<div class="user"><br> \
-                    <button class="prime">Добавить фото</button><br> \
-                    <button class="prime">Выйти<img src="images/exit.png"></button> \
-                    </div>';
+        autorizing: function(user) {
+            if (user !== null) {
+                let photoUser = document.createElement('img');
+                photoUser.className = 'main_photo';
+                document.body.appendChild(photoUser);
+                let newfont = document.createElement('font');
+                newfont.className = 'username';
+                newfont.innerHTML = user;
+                document.body.appendChild(newfont);
+                let container = document.createElement('div');
+                let container1 = document.createElement('div');
+                container1.className = 'user';
+                let add = document.createElement('button');
+                add.className = 'prime';
+                add.innerHTML = 'Добавить фото';
+                container1.appendChild(add);
+                let p = document.createElement('font');
+                p.innerHTML = '<br>';
+                container1.appendChild(p);
+                let exit = document.createElement('button');
+                exit.className = 'prime';
+                exit.innerHTML = 'Выйти';
+                container1.appendChild(exit);
+                container.appendChild(container1);
                 document.body.appendChild(container);
-    		}
-    		else {
-    			let hello = document.createElement('font');
-       		    hello.className = 'hello';
-       		    hello.innerHTML = 'Добро пожаловать!';
-       		    document.body.appendChild(hello);
-    			let container = document.createElement('div');
-       		    container.innerHTML = '<div class="user"><br><br> \
-                    <button class="prime">Войти</button> \
-                    </div>';
+            }
+             else {
+                let hello = document.createElement('font');
+                hello.className = 'hello';
+                hello.innerHTML = 'Добро пожаловать!';
+                document.body.appendChild(hello);
+                let container = document.createElement('div');
+                let container1 = document.createElement('div');
+                container1.className = 'user';
+                let p = document.createElement('div');
+                p.innerHTML = '<br><br>';
+                container1.appendChild(p);
+                let logIn = document.createElement('button');
+                logIn.className = 'prime';
+                logIn.innerHTML = 'Войти';
+                container1.appendChild(logIn);
+                container.appendChild(container1);
                 document.body.appendChild(container);
-    		}
-    	},
-    	doPhotopost: function(photoPost) {
-    		let post = document.createElement('div');
+            }
+        },
+        doPhotopost: function(photoPost) {
+            let post = document.createElement('div');
             post.className = 'post';
             post.id = photoPost.id;
             let photoUser = document.createElement('img');
             if (photoPost.author !== user) {
-       		    photoUser.className = 'photo';
-       		    photoUser.src = 'images/user.jpg'; 
-       		    post.appendChild(photoUser);
-       	    }
-       	    else {
-       		    photoUser.className = 'photo';
-       		    photoUser.src = 'images/authorized_user.jpg'; 
-       		    post.appendChild(photoUser);
-       	    } 
+                photoUser.className = 'photo';
+                photoUser.src = 'images/user.jpg'; 
+                post.appendChild(photoUser);
+            }
+            else {
+                photoUser.className = 'photo';
+                photoUser.src = 'images/authorized_user.jpg'; 
+                post.appendChild(photoUser);
+            } 
             let newfont = document.createElement('font');
-       		newfont.className = 'username';
-       		newfont.innerHTML = photoPost.author;
-       		post.appendChild(newfont);
-       		if (photoPost.author === user) {
-       			let instr = document.createElement('div');
-           		instr.className = 'instruments';
-           		let edit = document.createElement('font');
-           		edit.innerHTML = 'Редактировать';
-           		instr.appendChild(edit);
-           		let editpic = document.createElement('img');
-           		editpic.src = 'images/edit.png';
-           		edit.appendChild(editpic);
-           		instr.appendChild(edit);
-           		let p = document.createElement('font');
-           		p.innerHTML = '<br>';
-           		instr.appendChild(p);
-           		let dodelete = document.createElement('font');
-           		dodelete.innerHTML = 'Удалить';
-           		instr.appendChild(dodelete);
-           		let deletepic = document.createElement('img');
-           		deletepic.src = 'images/delete.png';
-           		dodelete.appendChild(deletepic);
-           		instr.appendChild(dodelete);
-           		post.appendChild(instr);
-       		}
+            newfont.className = 'username';
+            newfont.innerHTML = photoPost.author;
+            post.appendChild(newfont);
+            if (photoPost.author === user) {
+                let instr = document.createElement('div');
+                instr.className = 'instruments';
+                let edit = document.createElement('font');
+                edit.innerHTML = 'Редактировать';
+                instr.appendChild(edit);
+                let editpic = document.createElement('img');
+                editpic.src = 'images/edit.png';
+                edit.appendChild(editpic);
+                instr.appendChild(edit);
+                let p = document.createElement('font');
+                p.innerHTML = '<br>';
+                instr.appendChild(p);
+                let dodelete = document.createElement('font');
+                dodelete.innerHTML = 'Удалить';
+                instr.appendChild(dodelete);
+                let deletepic = document.createElement('img');
+                deletepic.src = 'images/delete.png';
+                dodelete.appendChild(deletepic);
+                instr.appendChild(dodelete);
+                post.appendChild(instr);
+            }
             const fordate = {
                 year: 'numeric',
                 month: 'long',
@@ -92,11 +112,11 @@ let DOM = (function() {
             let butt = document.createElement('button');
             butt.className = 'like';
             if (photoPost.likes !== undefined) {
-            	butt.innerHTML = photoPost.likes.length + '<br>likes';
+                butt.innerHTML = photoPost.likes.length + '<br>likes';
             }
-            else 
-            	butt.innerHTML = '0<br>likes';
-            
+            else {
+                butt.innerHTML = '0<br>likes';
+            }
             like.appendChild(butt);
             post.appendChild(like);
             let desc = document.createElement('div');
@@ -107,44 +127,45 @@ let DOM = (function() {
             hash.className = 'hashtag';
             hash.innerHTML = '#' + photoPost.hashTags;
             desc.appendChild(hash);
-    		return post;
-    	},
-    	showTape: function(photoPosts, start, end) {
+            return post;
+        },
+        showTape: function(photoPosts, start, end) {
             if (end !== undefined && start !== undefined) {
-                for (let i = 0; i < (end - start); i++) {
-                    if (i < photoPosts.length)
+                for (let i = 0; i < (end - start) && i < photoPosts.length; i++) {
                         content.appendChild(DOM.doPhotopost(photoPosts[i]));
                 }
-                let butt = document.createElement('button');
-                butt.className = 'more';
-                butt.innerHTML = 'Загрузить еще';
                 content.appendChild(butt);
                 document.body.appendChild(content);
             }
         },
         addPhotoPost: function(photoPost) {
-        	if (PhotoPortal.addPhotoPost(photoPost) === true) {
-        		content.insertBefore(DOM.doPhotopost(photoPost), content.firstElementChild);
-        		content.removeChild(content.lastElementChild);
-        		content.removeChild(content.lastElementChild);
-        		let butt = document.createElement('button');
-                butt.className = 'more';
-                butt.innerHTML = 'Загрузить еще';
-                content.appendChild(butt);
-        	}
+          if (PhotoPortal.addPhotoPost(photoPost) === true) {
+            content.insertBefore(DOM.doPhotopost(photoPost), content.firstElementChild);
+            content.removeChild(content.lastElementChild);
+            content.removeChild(content.lastElementChild);
+            content.appendChild(butt);
+          }
         },
         removePhotoPost: function(id) {
-        	if (PhotoPortal.removePhotoPost(id) === true) {
-        		let del = document.getElementById(id);
-        		content.removeChild(del);
-        		content.insertBefore(DOM.doPhotopost(PhotoPortal.getPhotoPost('19')), content.lastElementChild);
-        	}
+          content.removeChild(content.lastElementChild);
+            var foundIndex = PhotoPortal.getPhotoPosts(0, PhotoPortal.getLength()).findIndex(function(element) {
+                return element.id === content.lastElementChild.id;
+            });
+            if (PhotoPortal.removePhotoPost(id) === true) {
+                let del = document.getElementById(id);
+                content.removeChild(del);
+                content.appendChild(butt);
+                content.insertBefore(DOM.doPhotopost(PhotoPortal.getPhotoPosts(0, PhotoPortal.getLength())[foundIndex]), content.lastElementChild);
+                
+            }
+            else {
+              content.appendChild(butt);
+            }
         },
         editPhotoPost: function(id, photoPost) {
-        	if (PhotoPortal.editPhotoPost(id, photoPost) === true) {
-        		PhotoPortal.editPhotoPost(id, photoPost);
-        		content.replaceChild(DOM.doPhotopost(PhotoPortal.getPhotoPost(id)), document.getElementById(id));
-        	}
+            if (PhotoPortal.editPhotoPost(id, photoPost) === true) {
+                content.replaceChild(DOM.doPhotopost(PhotoPortal.getPhotoPost(id)), document.getElementById(id));
+            }
         },
         show: function() {
             DOM.autorizing(user);
@@ -152,11 +173,12 @@ let DOM = (function() {
             DOM.addPhotoPost({
             id: '21',
             description: '*шутка, что это не новый пост, я его стирала лаской*',
-            createdAt: new Date('2018-04-12T09:45:00'),
+            createdAt: new Date('2018-04-12T23:45:00'),
             author: 'lera',
             photoLink: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Mustela_nivalis_-British_Wildlife_Centre-4.jpg/1200px-Mustela_nivalis_-British_Wildlife_Centre-4.jpg',
             hashTags: ['happy','hurray']
             });
+            DOM.removePhotoPost('9');
             DOM.removePhotoPost('10');
             DOM.editPhotoPost('21', {
             id: '21',
@@ -165,8 +187,8 @@ let DOM = (function() {
             });
             DOM.editPhotoPost('21', {
             id: '21',
-            description: '',
-            hashTags: ['оп', 'оп']
+            description: 'aa',
+            hashTags: []
             });
         }
     };
