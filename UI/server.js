e19c1980a23820503d72840deeff6ef02ca56959
Shemplehova Yulia <ms.yulsh@mail.ru>
2018-03-27 10:53:52 +0300

    modified server.js

--- UI/server.js
+++ UI/server.js
@@ -31,18 +31,9 @@ let server = http.createServer((req, res) => {
 
     fs.readFile(filePath, function(error, content) { 
 	    if (error) { 
-		    if (error.code == 'ENOENT') { 
-		        fs.readFile('./404.html', function(error, content) { 
-		    	    res.writeHead(200, { 
-		            'Content-Type': contentType}); 
-		            res.end(content, 'utf-8'); 
-		        }); 
-		    } 
-		    else { 
-			    res.writeHead(500); 
-			    res.end('ERROR: ' + error.code + ' ..\n'); 
-			    res.end(); 
-		    } 
+	    	res.writeHead(500); 
+			res.end('ERROR: ' + error.code + ' ..\n'); 
+			res.end(); 
 	    } 
 	    else { 
 		    res.writeHead(200, {'Content-Type': contentType}); 
