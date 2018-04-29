4ed55341e4eccba440b60b5ceb782f000578bc4f
Shemplehova Yulia <ms.yulsh@mail.ru>
2018-04-29 20:03:58 +0300

    modified server

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const parser = require('body-parser');
 const fs = require('fs');
 
+const multer = require("multer");
+const upload = multer();
+
 let server = express();
 server.use(parser.json());
 server.use(express.static('public'));
@@ -23,11 +26,22 @@ server.get('/getPost/:id', (req, res) => {
 	post ? res.send(post) : res.status(404).end();
 });
 
+server.get('/getSize',(req, res) => {
+	let size = String(posts.getLength());
+	res.send(size);
+	res.status(200).end();
+});
+
+server.get('/putLike', (req, res) => {
+    posts.putlike(posts.getPhotoPost(req.query.id), req.query.user);
+	res.status(200).end();
+});
+
 server.post('/addPost', (req, res) => {
 	if (posts.addPhotoPost({
 		id: req.body.id,
         description: req.body.description,
-        createdAt: new Date(req.body.createdAt),
+        createdAt: req.body.createdAt,
         author: req.body.author,
         photoLink: req.body.photoLink,
         hashTags: req.body.hashTags,
@@ -37,8 +51,13 @@ server.post('/addPost', (req, res) => {
 		res.status(200).end();
 	}
 	else {
-		res.status(404).end();
+		res.status(406).end();
 	}
+});
+
+server.post('uploadImage', upload.single('file'), (req, res) => {
+    fs.writeFile("public/images" + req.file.originalname, req.file.buffer);
+    res.status(200).end();
 });
 
 server.delete('/deletePost/:id', (req, res) => {
