427afa05c5a7f178aede231cda33297433b90832
Shemplehova Yulia <ms.yulsh@mail.ru>
2018-05-27 15:41:52 +0300

    fixed task12

--- app.js
+++ app.js
@@ -30,27 +30,21 @@ server.get('/allPosts', (req, res) => {
   const photoposts = posts.getPhotoPosts(0, posts.getLength());
   if (photoposts.length) {
     res.send(photoposts);
-  } else {
-    res.status(404).end();
-  }
+  } else res.status(404).end();
 });
 
 server.post('/getPosts', (req, res) => {
   const photoposts = posts.getPhotoPosts(req.body.skip, req.body.top, req.body.filterConfig);
   if (photoposts.length) {
     res.send(photoposts);
-  } else {
-    res.status(404).end();
-  }
+  } else res.status(404).end();
 });
 
 server.get('/getPost/:id', (req, res) => {
   const post = posts.getPhotoPost(req.params.id);
   if (post) {
     res.send(post);
-  } else {
-    res.status(404).end();
-  }
+  } else res.status(404).end();
 });
 
 server.get('/getSize', (req, res) => {
@@ -60,9 +54,11 @@ server.get('/getSize', (req, res) => {
 });
 
 server.get('/putLike', (req, res) => {
-  const q = posts.putlike(posts.getPhotoPost(req.query.id), req.query.user);
-  res.send(String(q));
-  res.status(200).end();
+  if (req.user) {
+    const q = posts.putlike(posts.getPhotoPost(req.query.id), req.query.user);
+    res.send(String(q));
+    res.status(200).end();
+  } else res.status(404).end();
 });
 
 server.get('/findIndex/:id', (req, res) => {
@@ -70,9 +66,7 @@ server.get('/findIndex/:id', (req, res) => {
   if (ind !== -1) {
     res.send(String(ind));
     res.status(200).end();
-  } else {
-    res.status(404).end();
-  }
+  } else res.status(404).end();
 });
 
 server.get('/findPost/:index', (req, res) => {
@@ -80,51 +74,51 @@ server.get('/findPost/:index', (req, res) => {
   if (post !== undefined) {
     res.send(post);
     res.status(200).end();
-  } else {
-    res.status(404).end();
-  }
+  } else res.status(404).end();
 });
 
 server.post('/addPost', (req, res) => {
-  const post = {
-    id: req.body.id,
-    description: req.body.description,
-    createdAt: req.body.createdAt,
-    author: req.body.author,
-    photoLink: req.body.photoLink,
-    hashTags: req.body.hashTags,
-    likes: []
-  };
-  if (posts.addPost(post)) {
-    res.status(200).end();
-  } else {
-    res.status(404).end();
-  }
+  if (req.user) {
+    const post = {
+      id: req.body.id,
+      description: req.body.description,
+      createdAt: req.body.createdAt,
+      author: req.body.author,
+      photoLink: req.body.photoLink,
+      hashTags: req.body.hashTags,
+      likes: []
+    };
+    if (posts.addPost(post)) {
+      res.status(200).end();
+    } else res.status(404).end();
+  } else res.status(404).end();
 });
 
 server.post('/uploadImage', upload.single('file'), (req, res) => {
-  fs.writeFile('public/images/' + req.file.originalname, req.file.buffer);
-  res.status(200).end();
+  if (req.user) {
+    fs.writeFile('public/images/' + req.file.originalname, req.file.buffer);
+    res.status(200).end();
+  } else res.status(404).end();
 });
 
 server.delete('/deletePost/:id', (req, res) => {
-  if (posts.removePhotoPost(req.params.id)) {
-    res.status(200).end();
-  } else {
-    res.status(404).end();
-  }
+  if (req.user) {
+    if (posts.removePhotoPost(req.params.id)) {
+      res.status(200).end();
+    } else res.status(404).end();
+  } else res.status(404).end();
 });
 
 server.put('/editPost/:id', (req, res) => {
-  if (posts.editPhotoPost(req.params.id, {
-    description: req.body.description,
-    photoLink: req.body.photoLink,
-    hashTags: req.body.hashTags
-  })) {
-    res.status(200).end();
-  } else {
-    res.status(404).end();
-  }
+  if (req.user) {
+    if (posts.editPhotoPost(req.params.id, {
+      description: req.body.description,
+      photoLink: req.body.photoLink,
+      hashTags: req.body.hashTags
+    })) {
+      res.status(200).end();
+    } else res.status(404).end();
+  } else res.status(404).end();
 });
 
 server.post('/login', passport.authenticate('local'), (req, res) => {
